bbf1308b536800a7dc681a1023bc6c3007c7358e
Julian Pompey <julianpomp00@gmail.com>
2025-04-08 09:38:28 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+//https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
+/*
+1. Memory
+2. Display
+    How to go about creating a display? Need a popout console?
+        Research.
+3. Program Counter
+4. Index Register
+5. Stack
+6. Delay Timer
+7. Sound Timer
+8. Variable Register
+
+
+*/
